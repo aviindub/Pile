d9112d2b3443fb4758715bb3968f906ea156cae2
Avi Goldberg <aviindub@gmail.com>
2012-04-16 21:56:12 -0400

    added database calls and import corresponding to FragDatabaseModule.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,24 @@
-
 /*
  * GET home page.
  */
 
+var frags = require('FragDatabaseModule');
+ 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
 exports.user = function(req, res) {
-	var data = undefined;
-	res.render('user', data);
+	var username = req.params.user;
+	frags.getUserFrags(username, function(error, result) {
+		var data = {username: username, frags: result};
+		res.render('user', data);
+	});
 };
 
 exports.community = function(req, res) {
-	var data = undefined;
-	res.render('community', data);
+	frags.getCommunityFrags(function(error, result) {
+		var data = {frags: result};
+		res.render('community', data);
+	});
 };
