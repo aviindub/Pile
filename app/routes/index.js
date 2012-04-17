986900eb22e78073151317c1d50b73a4cacb0c8d
Avi Goldberg <aviindub@gmail.com>
2012-04-17 11:13:42 -0400

    minor stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,14 +11,14 @@ exports.index = function(req, res){
 exports.user = function(req, res) {
 	var username = req.params.user;
 	frags.getUserFrags(username, function(error, result) {
-		var data = {username: username, frags: result};
+		var data = {'username': username, 'frags': result};
 		res.render('user', data);
 	});
 };
 
 exports.community = function(req, res) {
 	frags.getCommunityFrags(function(error, result) {
-		var data = {frags: result};
+		var data = {'frags': result};
 		res.render('community', data);
 	});
 };
