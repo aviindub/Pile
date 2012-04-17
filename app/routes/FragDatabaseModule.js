bcc6bfbe0c0ae959971f6d8a7a34793dab1d5817
Avi Goldberg <aviindub@gmail.com>
2012-04-16 22:47:14 -0400

    might work. added recursive function for multi frag getting. need to figure out if i can pass sunion an array of keys.

--- app/routes/FragDatabaseModule.js
+++ app/routes/FragDatabaseModule.js
@@ -5,10 +5,39 @@
 var redis = require('redis');
 var client = redis.createClient();
 
-module.exports.getUserFrags = function (username, callback) {
-	callback(null, arrayOfFrags);
+function recGetFrags (array, nextIndex, results, callback) {
+	if (array.length === nextIndex) {
+		callback(null, results);
+	} else {
+		client.hgetall(array[nextIndex], function(error, result){
+			if (error) {
+				callback(error, null);
+			} else {
+				results.push(result);
+				recGetFrags(array, nextIndex+1, results, callback);
+			}
+		});
+	}
+}
+
+//callback gets results[] which contains frags
+function getFrags (fragIDs, callback) {
+	var results = [];
+	recMGetRange (fragIDs, 0, results, callback);
+}
+
+module.exports.getUserFrags = function(username, callback) {
+	client.smembers(username, function(error, fragIDs) {
+		getFrags(fragIDs, callback);
+	});
 };
 
-module.exports.getCommunityFrags = function (callback) {
-	callback(null, arrayOfFrags);
+module.exports.getCommunityFrags = function(callback) {
+	client.smembers('users', function(error, users) {
+		client.sunion(users, function(error, fragIDs) {  //unsure if kosher to pass array as 1st arg here instead of listing out keys
+			getFrags(fragIDs, callback);
+		});
+	});
 };
+
+
