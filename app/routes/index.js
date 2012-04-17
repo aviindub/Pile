e369685da188d488ecd76499e542d7d829d72d94
Avi Goldberg <aviindub@gmail.com>
2012-04-16 21:27:00 -0400

    added skeleton of route handling for user and community piles

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,3 +6,13 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.user = function(req, res) {
+	var data = undefined;
+	res.render('user', data);
+};
+
+exports.community = function(req, res) {
+	var data = undefined;
+	res.render('community', data);
+};
