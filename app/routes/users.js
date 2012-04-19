2c6dc1cb97f601d6ebf4caf7a722c54f3b1b4b55
Avi Goldberg <aviindub@gmail.com>
2012-04-19 12:34:16 -0400

    now creates sessions properly.

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,6 +4,7 @@ var users = {
 	'rebekah' : {login: 'rebekah', password: 'password', role: 'admin'}
 	'chris' : {login: 'chris', password: 'password', role: 'admin'}
 };
+/*
 module.exports.authenticate = function(login, password, callback) {
 	var user = users[login];
 	if (!user) {
@@ -15,4 +16,25 @@ module.exports.authenticate = function(login, password, callback) {
 		return;
 	}
 	callback(null);
+};
+*/
+
+module.exports.authenticate = function(login, password, callback) {
+	client.IsInSet(users, login, function(error, exists) {
+		if(exists) {
+			client.get(login, function(error, userInfo) {
+				if (error) {
+					console.log(error);
+					callback(error, null);
+				}
+				if (password === userInfo.password) {
+					req.session.authenticated = true;
+					req.session.user = login;
+					callback(null, true);
+				} else {
+					callback(null, false);
+				}
+			});
+		}
+	});
 };
