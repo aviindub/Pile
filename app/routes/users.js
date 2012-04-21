04b70f72d3ff818caec3c273ce2e3797855fa560
Avi Goldberg <aviindub@gmail.com>
2012-04-21 13:46:25 -0400

    rearranged for proper error handling. added comments

--- app/routes/users.js
+++ app/routes/users.js
@@ -23,23 +23,25 @@ module.exports.authenticate = function(login, password, callback) {
 	client.sismember('users', login, function(error, exists) {
 		if(exists === 1) {
 			client.hgetall(login, function(error, userInfo) {
-				
-				//reformat userInfo from array to hash
-				var hashify = {};
-				for (var i = 0; i < userInfo.length ; i += 2) {
-					hashify[userInfo[i]] = userInfo[i+1];
-				}
-				userInfo = hashify;
-				
 				if (error) {
 					console.log(error);
 					callback(error, null);
 				} else {
+				
+					//reformat userInfo from array to hash
+					var hashify = {};
+					for (var i = 0; i < userInfo.length ; i += 2) {
+						hashify[userInfo[i]] = userInfo[i+1];
+					}
+					userInfo = hashify;
+					
 					if (password === userInfo.password) {
+						//if password match, create session
 						req.session.authenticated = true;
 						req.session.user = login;
 						callback(null, true);
 					} else {
+						//no password match
 						callback(null, false);
 					}
 				}
