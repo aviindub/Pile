8d3fad0f8e9282aad0cc5ab163f51eb251dd60d5
Avi Goldberg <aviindub@gmail.com>
2012-04-21 12:36:09 -0400

    removed quotes from keys

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,7 +11,7 @@ exports.index = function(req, res){
 exports.user = function(req, res) {
 	var username = req.params.user;
 	frags.getUserFrags(username, function(error, result) {
-		var data = {'username': username, 'frags': result};
+		var data = {username: username, frags: result};
 		res.render('user', data);
 	});
 };
@@ -19,8 +19,8 @@ exports.user = function(req, res) {
 exports.pile = function(req, res) {
 	if(req.params.pile === 'community') {
 		frags.getCommunityFrags(function(error, result) {
-			var data = {'frags': result};
-			res.render('community', data);
+			var data = {frags: result};
+			res.render(community, data);
 		});
 	} else {
 		//other piles
