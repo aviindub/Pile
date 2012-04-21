317b129627cce2826e905c64afd70178a6edd97c
Avi Goldberg <aviindub@gmail.com>
2012-04-21 13:24:30 -0400

    added comments

--- app/routes/piles.js
+++ app/routes/piles.js
@@ -3,6 +3,7 @@
 */
 
 exports.user = function(req, res) {
+	//route for individual user piles
 	var username = req.params.user;
 	frags.getUserFrags(username, function(error, result) {
 		var data = {username: username, frags: result};
@@ -11,6 +12,7 @@ exports.user = function(req, res) {
 };
 
 exports.pile = function(req, res) {
+	//route for group piles
 	if(req.params.pile === 'community') {
 		frags.getCommunityFrags(function(error, result) {
 			var data = {frags: result};
@@ -18,5 +20,6 @@ exports.pile = function(req, res) {
 		});
 	} else {
 		//other piles
+		//or just an error page
 	}
 };
