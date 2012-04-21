cdaaf6e767f70350e4ebb1e6ed6ed71623b29d60
Avi Goldberg <aviindub@gmail.com>
2012-04-21 13:17:44 -0400

    moved pile related routes to their own file

---
+++ app/routes/piles.js
@@ -0,0 +1,22 @@
+/*
+* pile related routes
+*/
+
+exports.user = function(req, res) {
+	var username = req.params.user;
+	frags.getUserFrags(username, function(error, result) {
+		var data = {username: username, frags: result};
+		res.render('user', data);
+	});
+};
+
+exports.pile = function(req, res) {
+	if(req.params.pile === 'community') {
+		frags.getCommunityFrags(function(error, result) {
+			var data = {frags: result};
+			res.render(community, data);
+		});
+	} else {
+		//other piles
+	}
+};
