e81dc7b5e1265a18601caecd48063b7ffb4eb0d0
Avi Goldberg <aviindub@gmail.com>
2012-04-27 21:47:35 -0400

    added createFrag, and filled out saveUserPile.

--- app/routes/piles.js
+++ app/routes/piles.js
@@ -1,12 +1,12 @@
 /*
-* pile related routes
+* pile and frag related routes
 */
 var frags = require('./FragDatabaseModule');
 
 
 exports.user = function(req, res) {
 	//route for individual user piles
-	var username = req.params.user;
+	var username = req.session.user;
 	frags.getUserFrags(username, function(error, result) {
 		var data = {username: username, frags: result};
 		res.render('user', data);
@@ -28,4 +28,14 @@ exports.pile = function(req, res) {
 
 exports.saveUserPile = function(req, res) {
 	//handle AJAX post with updated frag positions
+	var data = JSON.parse(req.param('saveData', null);
+	frags.saveFragPositions(data, req.session.user);
+}
+
+exports.createFrag = function(req, res) {
+	//handle POST of new frag data
+	frags.createFrag(req.param('content', null), req.session.user, function(error) { 
+		//add error handling
+		res.redirect('/piles/' + req.session.user);
+	});
 }
