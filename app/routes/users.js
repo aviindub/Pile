7fa166f7eec0821a44780c94c69a8eb86115873e
Avi Goldberg <aviindub@gmail.com>
2012-04-27 21:49:09 -0400

    removed users variable since we are now using the database for that. moved old code to be deleted down to the bottom.

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,24 +1,3 @@
-var users = {
-	'avi' : {login: 'avi', password: 'password', role: 'admin'},
-	'david' : {login: 'david', password: 'password', role: 'admin'}
-	'rebekah' : {login: 'rebekah', password: 'password', role: 'admin'}
-	'chris' : {login: 'chris', password: 'password', role: 'admin'}
-};
-/*
-module.exports.authenticate = function(login, password, callback) {
-	var user = users[login];
-	if (!user) {
-		callback(null);
-		return;
-	}
-	if (user.password == password) {
-		callback(user);
-		return;
-	}
-	callback(null);
-};
-*/
-
 module.exports.authenticate = function(login, password, callback) {
 	client.sismember('users', login, function(error, exists) {
 		if(error) {
@@ -57,3 +36,26 @@ module.exports.authenticate = function(login, password, callback) {
 		}
 	});
 };
+
+
+/* OLD STUFF... delete
+var users = {
+	'avi' : {login: 'avi', password: 'password', role: 'admin'},
+	'david' : {login: 'david', password: 'password', role: 'admin'}
+	'rebekah' : {login: 'rebekah', password: 'password', role: 'admin'}
+	'chris' : {login: 'chris', password: 'password', role: 'admin'}
+};
+
+module.exports.authenticate = function(login, password, callback) {
+	var user = users[login];
+	if (!user) {
+		callback(null);
+		return;
+	}
+	if (user.password == password) {
+		callback(user);
+		return;
+	}
+	callback(null);
+};
+*/
